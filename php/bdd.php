ed029951a8888ed9c7a9269bedfd0a0d911a193e
Marian00b <marianne.borderes@gmail.com>
2016-11-16 12:35:20 +0100

    creation bdd

--- php/bdd.php
+++ php/bdd.php
@@ -66,4 +66,6 @@ function vidage_table() {
 	requete($maBd, $maRequeteVidage);
 }
 
+
+
 ?>
---
+++ php/creation_bdd.php
@@ -0,0 +1,10 @@
+<?php
+include("bdd.php");
+
+creation_table(); 
+insertion_exemples();
+//use Annonces;
+//show tables;
+//describe annonces;
+//select * from annonces;
+?>
